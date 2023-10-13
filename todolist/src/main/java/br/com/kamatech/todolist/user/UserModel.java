faef4caad3aa3d0f08495170a84399e5ef8d2eaa
fkamag <fabiokamagian@gmail.com>
2023-10-13 17:33:23 -0300

    alterando atributos de public para private e criando métodos getter e setter

--- todolist/src/main/java/br/com/kamatech/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/kamatech/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package br.com.kamatech.todolist.user;
 
 public class UserModel {
-  public String username;
-  public String name;
-  public String password;
+  private String username;
+  private String name;
+  private String password;
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUsername() { 
+    return username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
 }
