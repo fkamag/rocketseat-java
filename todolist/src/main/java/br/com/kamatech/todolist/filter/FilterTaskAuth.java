853326b3f137afbf76e9359bbbdb80a9819b7f85
fkamag <fabiokamagian@gmail.com>
2023-10-13 23:54:20 -0300

    recebendo usuário e senha no request.Auth

--- todolist/src/main/java/br/com/kamatech/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/kamatech/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,45 @@
 package br.com.kamatech.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth  implements Filter{
+public class FilterTaskAuth  extends OncePerRequestFilter{
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+        var authorization = request.getHeader("authorization");
+
+        // retirando a palavra Basic e o espaço
+        var user_password = authorization.substring("Basic".length()).trim();
+
+        // convertendo o array de bytes
+        byte[] authDecode = Base64.getDecoder().decode(user_password);
+
+        // convertendo o array de byte em String
+        var authString = new String(authDecode);
+
+        // separando username e password
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+
+        filterChain.doFilter(request, response);
     
-    System.out.println("Chegou no filtro");
-    chain.doFilter(request, response);
   }
+
   
 }
